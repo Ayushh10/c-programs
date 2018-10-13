aefe8eb76771aaf8e72131d0ad879a7182ae0dc1
Ayush <43788474+Ayushh10@users.noreply.github.com>
2018-10-13 20:53:16 +0530

    calculate the roots of quadraatic equation using shridharacharya method.

---
+++ roots.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+ void main()
+{
+ int a,b,c;
+ float r1,r2;
+ printf("enter value of a \n");
+ scanf("%d", &a);
+ printf("enter value of b \n");
+ scanf("%d", &b);
+ printf("enter value of c \n");
+ scanf("%d", &c);
+ printf("the entered equation is:");
+ printf("%dx^2+",a);
+ printf("%dy+",b);
+ printf("%d \n",c);
+ printf("the required roots are: \n");
+ r1=((-b+sqrt(4*a*c)))/2*a;
+ r2=((-b-sqrt(4*a*c)))/2*a;
+ printf("roots are: \n");
+ printf("%f , %f", r1, r2);
+ getch();
+}
