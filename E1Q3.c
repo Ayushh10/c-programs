e1e79d30c6cb527e5f148fb70e814e06a64e5ba5
Ayush <43788474+Ayushh10@users.noreply.github.com>
2019-02-07 12:28:13 +0530

    This program accepts the radius of a circle and returns Area and Circumference.
    This program accepts radius and gives Area and Circumference of the corresponding circle.  The '.exe' is a directly executable file of this program and the '.o' is the object file of the program.

---
+++ E1Q3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int r;
+ float c,a;
+ printf("enter the radius a circle ");
+ scanf("%d",&r);
+ a=2*3.14*r*r;
+ c=2*3.14*r*r*r;
+ printf("\n area is %f",a);
+ printf("\n circumference is %f",c);
+ getch();
+ }
