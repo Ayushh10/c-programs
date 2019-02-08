faced38b86ba016a4c821805840f7b2f84724cc8
Ayush <43788474+Ayushh10@users.noreply.github.com>
2019-02-08 20:45:24 +0530

    This program accepts temperature in Celsius and converts it into Fahrenheit.
    This program accepts temperature in Celsius and converts it into Fahrenheit. The '.exe' is the directly executable file from the command prompt. The '.o' is the object file of this program. And ',c' is the C-file of this program.

---
+++ E1Q4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float C,F;
+ printf("enter the temperature (in C) ");
+ scanf("%f",&C);
+ F=(C*9/5)+32;
+ printf("\n the temp. in F is %f",F);
+ getch();
+ }
